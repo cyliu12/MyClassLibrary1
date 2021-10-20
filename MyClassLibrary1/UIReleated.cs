161fcc87711d9ddf9ea88dfd049fe0bc627dea48
cyliu12 <cyliu12@gmail.com>
2021-10-20 09:51:15 +0800

    DoEvents

---
+++ MyClassLibrary1/UIReleated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Permissions;
+using System.Windows.Threading;
+
+namespace MyClassLibrary1
+{
+    static public class UIReleated
+    {
+        [SecurityPermissionAttribute(SecurityAction.Demand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        static public void DoEvents()
+        {
+            var frame = new DispatcherFrame();
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background,
+                new DispatcherOperationCallback(ExitFrame), frame);
+            Dispatcher.PushFrame(frame);
+        }
+
+        static public object ExitFrame(object f)
+        {
+            ((DispatcherFrame)f).Continue = false;
+
+            return null;
+        }
+
+        static public void test()
+        {
+            
+        }
+    }
+}
