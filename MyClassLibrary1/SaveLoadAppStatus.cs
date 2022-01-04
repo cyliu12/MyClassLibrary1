0ef83980543db71cc93dcba2996c43dd0fa8cb55
cyliu12 <cyliu12@gmail.com>
2022-01-04 17:30:12 +0800

    working

--- MyClassLibrary1/SaveLoadAppStatus.cs
+++ MyClassLibrary1/SaveLoadAppStatus.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace MyClassLibrary1
 {
-    public class SaveLoadAppStatus
+    public static class SaveLoadAppStatus
     {
+        public static void tester()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+
+    [Serializable()]
+    public class SaveObjects
+    {
+        private static Dictionary<string,string> _stock;
+
+        public Dictionary<string,string> Stock
+        {
+            get { return _stock; }
+        }
+
+
+        public static void AddItem(string name, string valueString)
+        {
+            _stock.Add(name, valueString);
+        }
+       
+
     }
 }
---
+++ Tester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary1;
+
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveLoadAppStatus.tester();
+            SaveObjects saveObjects = new SaveObjects();
+            SaveObjects.AddItem("number1", "10");
+            Console.WriteLine(aveObjects.Stock["number1"]);
+            Console.ReadLine();
+        }
+    }
+}
