64a76418fb7adcecb6d11a0f66d65aa7bd5b4331
劉 駿逸 <cyliu12@gmail.com>
2023-02-25 23:39:09 +0800

    add Class StringExt

---
+++ MyClassLibrary1/StringExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary1
+{
+    public static class StringExt
+    {
+        static public bool IsNumeric(string value)
+        {
+            return value.All(char.IsNumber);
+        }
+    }
+}
